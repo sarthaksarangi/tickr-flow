62b7e1c23f574b6a38f18a4bea339120e992a715
sarthaksarangi <sarthakrohan5@gmail.com>
2025-11-23 22:16:35 +0530

    Implemented header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,8 @@ import {Button} from "@/components/ui/button";
 
 const Home = () => {
     return (
-        <div className = 'flex justify-center'>
-            <Button>Click me </Button>
+        <div className='flex min-h-screen home-wrapper'>
+            Home
         </div>
     )
 }
---
+++ components/UserDropdown.tsx
@@ -0,0 +1,92 @@
+'use client'
+import React from 'react'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
+import {useRouter} from "next/navigation";
+import {Button} from "@/components/ui/button";
+import {LogOut} from "lucide-react";
+import NavItems from "@/components/NavItems";
+
+const UserDropdown = () => {
+    const router = useRouter();
+    const handleSignout = async () => {
+        router.push('/sign-in')
+    }
+
+    const user = {
+        name: 'jhon',
+        email: 'sarthaksarangi@email.com'
+    }
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant='ghost'
+                        className='flex items-center gap-3 text-gray-400 hover:text-yellow-500'>
+
+                    <Avatar className="rounded-lg">
+                        <AvatarImage
+                            src="https://github.com/evilrabbit.png"
+                            alt="@evilrabbit"
+                        />
+                        <AvatarFallback
+                            className='bg-yellow-500 text-yellow-500 text-sm font-bold'>{user.name[0]}</AvatarFallback>
+                    </Avatar>
+                    <div className="hidden md:flex flex-col items-start ">
+                        <span className='text-base font-medium text-gray-400'>
+                            {user.name}
+                        </span>
+                    </div>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="text-gray-400">
+                <DropdownMenuLabel>
+                    <div className="flex relative items-center gap-3 py-2">
+                        <Avatar className="h-10 w-10">
+                            <AvatarImage
+                                src="https://github.com/evilrabbit.png"
+                                alt="@evilrabbit"
+                            />
+                            <AvatarFallback
+                                className='bg-yellow-500 text-yellow-500 text-sm font-bold'>{user.name[0]}</AvatarFallback>
+                        </Avatar>
+                        <div className="flex flex-col ">
+                        <span className='text-base font-medium text-gray-400'>
+                            {user.name}
+                        </span>
+                            <span className='text-sm text-gray-500'>
+                            {user.email}
+                        </span>
+                        </div>
+                    </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator className='bg-gray-600'/>
+                <DropdownMenuItem onClick={() => handleSignout()}
+                                  className='text-gray-100 text-md font-medium focus:text-yellow-500 focus:bg-transparent transition-colors cursor-pointer'>
+                    <LogOut className='h-4 w-4 mr-2 hidden sm:block'/>
+                    Logout
+                </DropdownMenuItem>
+                <nav className='sm:hidden'>
+                    <NavItems/>
+                </nav>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+export default UserDropdown
