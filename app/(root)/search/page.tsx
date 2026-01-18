ad9cb8577c98961d31fc360ebe8799388694a15b
sarthaksarangi <sarthakrohan5@gmail.com>
2026-01-18 23:16:14 +0530

    Deleted search componrnt

--- app/(root)/search/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import SearchCommand from "@/components/SearchCommand";
-
-const Page = () => {
-    return (
-        <div><SearchCommand/></div>
-    )
-}
-export default Page
